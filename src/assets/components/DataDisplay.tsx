c1f71d28ad7359b364fb5a698825055b57e2341c
Piootrekk <piotr.wilkowiecki100@gmail.com>
2024-02-06 22:01:58 +0100

    selected items & prepare to price parse

--- src/assets/components/DataDisplay.tsx
+++ src/assets/components/DataDisplay.tsx
@@ -5,21 +5,65 @@ import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import { Styles } from "./DataDisplayStyles";
+import { useState } from "react";
+import { Button } from "@mui/material";
 
 interface DataDisplayProps {
   assets: FinalAssets[];
 }
 
 const DataDisplay: React.FC<DataDisplayProps> = ({ assets }) => {
+  const [selectedToCheckPrice, setSelectedToCheckPrice] = useState<
+    FinalAssets[]
+  >([]);
+
+  const handleCardClick = (selectedAsset: FinalAssets) => {
+    const isAssetSelected = selectedToCheckPrice.some(
+      (asset: FinalAssets) => asset.key === selectedAsset.key
+    );
+    if (isAssetSelected) {
+      const updatedSelectedAssets = selectedToCheckPrice.filter(
+        (asset: FinalAssets) => asset.key !== selectedAsset.key
+      );
+      setSelectedToCheckPrice(updatedSelectedAssets);
+    } else {
+      setSelectedToCheckPrice((prevSelectedAssets: FinalAssets[]) => [
+        ...prevSelectedAssets,
+        selectedAsset,
+      ]);
+    }
+  };
+
   return (
     <Box sx={Styles}>
+      <Button
+        variant="contained"
+        sx={{
+          visibility: selectedToCheckPrice.length > 0 ? "visible" : "hidden",
+        }}
+      >
+        Load Price Selected Items: {selectedToCheckPrice.length}
+      </Button>
+
       <Grid container spacing={2}>
         {assets.map((asset: FinalAssets) => (
           <Grid item key={asset.key}>
             <Card
               sx={{
                 backgroundColor: `#${asset.name_color}`,
+                "&:hover": {
+                  cursor: asset.marketable === 1 ? "pointer" : "not-allowed",
+                },
+
+                "&::before": {
+                  border: selectedToCheckPrice.some(
+                    (selectedAsset) => selectedAsset.key === asset.key
+                  )
+                    ? "3px solid blue"
+                    : "none",
+                },
               }}
+              onClick={() => handleCardClick(asset)}
             >
               <img
                 src={`https://community.cloudflare.steamstatic.com/economy/image/${asset.icon_url}`}
--- src/assets/components/DataDisplayStyles.ts
+++ src/assets/components/DataDisplayStyles.ts
@@ -1,11 +1,12 @@
 // DataDisplayStyles.ts
 
 export const Styles = {
-  margin: "3rem",
+  margin: "1rem",
   padding: "0.5rem",
   boxShadow: "0 2px 5px rgba(0,0,0,0.5)",
   borderRadius: "10px",
   textAlign: "center",
+  position: "relative",
 
   "& .MuiGrid-container": {
     justifyContent: "center",
@@ -14,16 +15,33 @@ export const Styles = {
 
   "& .MuiGrid-item": {},
   "& .MuiCard-root": {
+    transform: "scale(1) rotate(0)",
+    boxShadow: "0 0 0 rgba(0,2,3,0)",
+    transition: "transform 0.3s",
+    position: "relative",
     padding: "0.5rem",
-    transition: "transform 0.3s, rotate 0.3s 0.5s, box-shadow 0.3s",
+
+    "&::before": {
+      content: "''",
+      position: "absolute",
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+    },
+
     "&:hover": {
       transform: "scale(1.15) rotate(2deg)",
-      boxShadow: "0 5px 15px rgba(0,0,0,0.5)",
+      boxShadow: "0 5px 5px rgba(0,2,3,1)",
     },
   },
   "& img": {
     width: "6rem",
     height: "6rem",
     margin: "auto",
+  },
+  "& .MuiButtonBase-root": {
+    padding: "0.75rem",
+    marginBottom: "2rem",
   },
 };
